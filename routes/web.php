5527c5ac90ab2bb15bcd4aed6a81d9476dbcddce
WeiYouYiLin <1450580734@qq.com>
2019-04-26 09:57:05 +0000

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     // 生成众筹订单
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+    // 创建分期付款接口
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
