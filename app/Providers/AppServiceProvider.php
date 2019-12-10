d7c29d8b739cf4c033dbd7698a997d70b67440e7
WeiYouYiLin <1450580734@qq.com>
2019-12-10 01:55:18 +0000

    horizon permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,21 @@ class AppServiceProvider extends ServiceProvider
                 \Log::info(Str::replaceArray('?', $query->bindings, $query->sql));
             });
         }
+        // horizon 访问权限
+        Horizon::auth(function ($request) {
+            // 这里进行判断，根据需求自行选择
+
+            // 通过认证可以访问
+            if (Auth::check()) {
+                return true;
+            }
+
+            // 指定参数可以访问
+            if ($request->input('q') == 'horizon') {
+                return true;
+            }
+
+        });
     }
 
     /**
